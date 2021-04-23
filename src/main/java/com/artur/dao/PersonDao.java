cd55ddd9469267df83a5f5fc7fd5d4c1efc303d1
ArturVazniak <artur.vozniak@gmail.com>
2021-04-23 19:53:09 +0300

    Application SpringMVC_v1.0

---
+++ src/main/java/com/artur/dao/PersonDao.java
@@ -0,0 +1,45 @@
+package com.artur.dao;
+
+import com.artur.models.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PersonDao {
+    private static int PEOPLE_COUNT;
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public PersonDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Person> index() {
+     return jdbcTemplate.query("SELECT * FROM Person", new PersonMapper());
+    }
+
+    public Person show(int id) {
+      return (Person) jdbcTemplate.query("SELECT * FROM Person WHERE id=?", new Object[]{id}, new PersonMapper())
+              .stream().findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        jdbcTemplate.update("INSERT INTO Person VALUES (1, ?, ?, ?)",
+               person.getName(), person.getAge(), person.getEmail());
+    }
+
+    public void update(int id, Person updatedPerson) {
+      jdbcTemplate.update("UPDATE Person SET name=?, age=?, email=? WHERE id=?",
+              updatedPerson.getName(), updatedPerson.getAge(), updatedPerson.getEmail(), id);
+    }
+
+    public void delete(int id) {
+
+       jdbcTemplate.update("DELETE FROM Person WHERE id=?", id);
+
+    }
+}
